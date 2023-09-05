cd6662cb91c7561b6be0c04d2b2f4035ed1f0954
Eugenia <costamariaeugenia1@gmail.com>
2023-09-05 19:44:21 -0300

    subo la clase 3

---
+++ 2do_anio_2do_sem/laboratorio_programacion/javascript/clase3/client/js/products.js
@@ -0,0 +1,50 @@
+const productos = [
+  {
+    id: 1,
+    productName: 'Banana',
+    price: 480,
+    quantity: 1,
+    img: '/client/assets/banana.png',
+    alt: 'banana'
+  },
+  {
+    id: 2,
+    productName: 'Leche',
+    price: 950,
+    quantity: 1,
+    img: '/client/assets/milk.png',
+    alt: 'leche'
+  },
+  {
+    id: 3,
+    productName: 'Pollo',
+    price: 750,
+    quantity: 1,
+    img: '/client/assets/chiken.png',
+    alt: 'pollo'
+  },
+  {
+    id: 4,
+    productName: 'Ketchup',
+    price: 700,
+    quantity: 1,
+    img: '/client/assets/ketchup.png',
+    alt: 'ketchup'
+  },
+  {
+    id: 5,
+    productName: 'Lays',
+    price: 950,
+    quantity: 1,
+    img: '/client/assets/lays.png',
+    alt: 'lays'
+  },
+  {
+    id: 6,
+    productName: 'Atun',
+    price: 1200,
+    quantity: 1,
+    img: '/client/assets/tuna-tin.png',
+    alt: 'lata de atun'
+  }
+]
